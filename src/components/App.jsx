79286af91fe275bdb549140dc8110f900b06c3d6
zfedorovska <43138422+zfedorovska@users.noreply.github.com>
2022-05-09 16:26:21 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,6 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
 import Searchbar from 'components/Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
-import Modal from 'components/Modal/Modal'
 
 export default class App extends Component {
   state = {
